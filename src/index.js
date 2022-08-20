bafa7ab97cd7612ea9d6482478f0c3687d0de0cb
ValeriiaKov <l380979478175k@gmail.com>
2022-08-20 11:45:12 +0300

    removed function showCurrent and made both functions in one, new functions that changing units and temperature in both sides

--- src/index.js
+++ src/index.js
@@ -21,44 +21,12 @@ if (minutes < 10) {
 let currentDate = document.querySelector("#today-time");
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 
-function changeCurrent(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = `${temperature}°C`;
+function changeWeather(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  let windSpeed = Math.round(response.data.wind.speed);
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = `Wind: ${windSpeed} km/h`;
-  let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].main;
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].main);
-}
-function getGeolocation(position) {
-  let lat = `${position.coords.latitude}`;
-  let lon = `${position.coords.longitude}`;
-  let apiKey = "ae23c8dd3319699701cfabe88335a92f";
-  let apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrlCurrent).then(changeCurrent);
-}
-function callNavigator() {
-  navigator.geolocation.getCurrentPosition(getGeolocation);
-}
-let currentButton = document.querySelector("#current-button");
-currentButton.addEventListener("click", callNavigator);
-
-function changeWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temp = document.querySelector("#current-temp");
-  temp.innerHTML = `${temperature}°C`;
+  celsiusTemp = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector("#temp-value");
+  currentTemp.innerHTML = celsiusTemp;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let windSpeed = Math.round(response.data.wind.speed);
@@ -73,15 +41,51 @@ function changeWeather(response) {
   );
   iconElement.setAttribute("alt", `$response.data.weather[0].main`);
 }
+
+function getGeolocation(position) {
+  let lat = `${position.coords.latitude}`;
+  let lon = `${position.coords.longitude}`;
+  let apiKey = "ae23c8dd3319699701cfabe88335a92f";
+  let apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrlCurrent).then(changeWeather);
+}
+function callNavigator() {
+  navigator.geolocation.getCurrentPosition(getGeolocation);
+}
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", callNavigator);
+
 function changeCity(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#searchcity-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchCity.value;
   let apiKey = "ae23c8dd3319699701cfabe88335a92f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(changeWeather);
 }
+let celsiusTemp = null;
 
 let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", changeCity);
+
+function showFahreheitTemp(event) {
+  event.preventDefault();
+  let fahrenheitElement = Math.round((celsiusTemp * 9) / 5 + 32);
+  let currentTemp = document.querySelector("#temp-value");
+  currentTemp.innerHTML = fahrenheitElement;
+  celsiusLink.classList.remove("activePage");
+  fahrenheitLink.classList.add("activePage");
+}
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#temp-value");
+  currentTemp.innerHTML = celsiusTemp;
+  celsiusLink.classList.add("activePage");
+  fahrenheitLink.classList.remove("activePage");
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-temperature");
+fahrenheitLink.addEventListener("click", showFahreheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-temperature");
+celsiusLink.addEventListener("click", showCelsiusTemp);
